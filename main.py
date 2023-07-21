f43ab5f70e183528ebab90e26151da35083f9725
Ruben van Breda <re.vanbreda@gmail.com>
2023-07-21 13:11:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,8 @@ def chat(df, prompt):
 
 def chat_with_data(df, prompt):
     llm = OpenAI(api_token=API_KEY)
-    pandas_ai = PandasAI(llm, middlewares=[StreamlitMiddleware()],save_charts=True, save_charts_path=f"{git_path}" , enforce_privacy=True)
+    # save_charts=True, save_charts_path=f"{git_path}"
+    pandas_ai = PandasAI(llm, middlewares=[StreamlitMiddleware()] , enforce_privacy=True)
     response = pandas_ai.run(df, prompt=prompt)
     return response
 
